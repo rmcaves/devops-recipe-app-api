30450cb45701648ceb49d1bf25b3d1cade5e230e
Romeo Cavestany <rmcavestany@gmail.com>
2024-08-10 17:35:58 -0700

    updated polict document fo iam in setup/iam.tf

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -241,7 +241,10 @@ data "aws_iam_policy_document" "iam" {
       "iam:AttachRolePolicy",
       "iam:TagRole",
       "iam:TagPolicy",
-      "iam:PassRole"
+      "iam:PassRole",
+      "iam:CreateServiceLinkedRole",
+      "iam:DeleteServiceLinkedRole",
+      "iam:GetServiceLinkedRoleDeletionStatus"
     ]
     resources = ["*"]
   }
